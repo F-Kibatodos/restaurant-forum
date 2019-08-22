6aebc8adbf5126b50bfb16d35222daf75bd99dd5
Brian.L <brian9500885@gmail.com>
2019-08-23 00:46:51 +0800

    add restaurant index page

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 新增引擎
+app.set('view engine', 'handlebars') // 使用引擎
+
+require('./routes')(app)
 
 app.listen(3000)
