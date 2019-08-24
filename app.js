49baa15c4a293228973886d240e3e18b9682d101
Brian.L <brian9500885@gmail.com>
2019-08-24 16:02:27 +0800

    install bcrypt-nodejs, body-parser and add userController and signup page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models') // 引入資料庫
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 新增引擎
 app.set('view engine', 'handlebars') // 使用引擎
+app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./routes')(app)
 
