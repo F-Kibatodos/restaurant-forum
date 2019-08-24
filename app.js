2f2d1fb4b3db9cfc3f75e6151ec235ba899c0be5
Brian.L <brian9500885@gmail.com>
2019-08-24 11:39:59 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 新增引擎
 app.set('view engine', 'handlebars') // 使用引擎
 
 require('./routes')(app)
 
-app.listen(3000)
+app.listen(3000, () => {
+  db.sequelize.sync() // 跟資料庫同步
+})
