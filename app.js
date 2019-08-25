0f7eaee4783c665d99bca121aa0f606693798497
Brian.L <brian9500885@gmail.com>
2019-08-25 10:46:24 +0800

    Install express-session connect-flash and confirm user password and unique email

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models') // 引入資料庫
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 新增引擎
 app.set('view engine', 'handlebars') // 使用引擎
 app.use(bodyParser.urlencoded({ extended: true }))
+// setup session and flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 require('./routes')(app)
 
