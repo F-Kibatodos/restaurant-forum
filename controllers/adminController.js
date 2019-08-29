1b051fcb5883ecdc80357248fb360f209856eea9
Brian.L <brian9500885@gmail.com>
2019-08-29 10:04:36 +0800

    Show category name on admin/restaurants and Show category name on admin/restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,12 +2,13 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
 const fs = require('fs')
+const Category = db.Category
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll().then(restaurants => {
+    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
   },
@@ -51,11 +52,13 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id).then(restaurant => {
-      return res.render('admin/restaurant', {
-        restaurant: restaurant
-      })
-    })
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(
+      restaurant => {
+        return res.render('admin/restaurant', {
+          restaurant: restaurant
+        })
+      }
+    )
   },
   editRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id).then(restaurant => {
