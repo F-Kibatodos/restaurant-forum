ed8b28dbe8f33d853a6cf81945f2329884c52dbf
Brian.L <brian9500885@gmail.com>
2019-08-29 12:21:26 +0800

    Add admin categories page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -148,6 +148,11 @@ const adminController = {
           res.redirect('/admin/users')
         })
     })
+  },
+  getCategories: (req, res) => {
+    return Category.findAll.then(categories => {
+      return res.render('categories', { categories })
+    })
   }
 }
 
