22a50d37b999cf818fdc4b6a01017612f9950cfb
Brian.L <brian9500885@gmail.com>
2019-09-02 16:18:10 +0800

    refactor router and GET /api/admin/restaurants

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   next()
 })
 
-require('./routes')(app, passport)
+require('./routes')(app)
 app.use('/upload', express.static(__dirname + '/upload'))
 
 app.listen(process.env.PORT || 3000, () => {
